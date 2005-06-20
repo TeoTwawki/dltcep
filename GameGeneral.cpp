83272e3416e64d04a4f9d20c8f08fceca819aaa0
Avenger <avenger_teambg@users.sourceforge.net>
2005-06-20 22:51:35 +0000

    Added some missing files to dltcep source

---
+++ GameGeneral.cpp
@@ -0,0 +1,127 @@
+// GameGeneral.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "chitem.h"
+#include "GameGeneral.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CGameGeneral dialog
+
+
+CGameGeneral::CGameGeneral(CWnd* pParent /*=NULL*/)
+	: CDialog(CGameGeneral::IDD, pParent)
+{
+	//{{AFX_DATA_INIT(CGameGeneral)
+		// NOTE: the ClassWizard will add member initialization here
+	//}}AFX_DATA_INIT
+}
+
+
+void CGameGeneral::DoDataExchange(CDataExchange* pDX)
+{
+  CButton *cb;
+  CString tmpstr;
+  int tmp;
+  int i,j;
+
+	CDialog::DoDataExchange(pDX);
+
+  DDX_Text(pDX, IDC_GAMETIME, the_game.header.gametime);
+  DDX_Text(pDX, IDC_GOLD, the_game.header.gold);
+
+  RetrieveResref(tmpstr,the_game.header.mainarea);
+  DDX_Text(pDX, IDC_STARTAREA, tmpstr);
+  StoreResref(tmpstr,the_game.header.mainarea);
+
+  DDX_Text(pDX, IDC_WEATHER, the_game.header.weather2);
+
+  tmp=the_game.header.weather2;
+  j=1;
+  for(i=0;i<8;i++) {
+      cb=(CButton *) GetDlgItem(IDC_FLAG1+i);
+      if(cb) cb->SetCheck(!!(tmp&j) );
+      j<<=1;
+  }
+}
+
+
+BEGIN_MESSAGE_MAP(CGameGeneral, CDialog)
+	//{{AFX_MSG_MAP(CGameGeneral)
+	ON_EN_KILLFOCUS(IDC_STARTAREA, OnDefaultKillfocus)
+	ON_BN_CLICKED(IDC_FLAG1, OnFlag1)
+	ON_BN_CLICKED(IDC_FLAG2, OnFlag2)
+	ON_BN_CLICKED(IDC_FLAG3, OnFlag3)
+	ON_BN_CLICKED(IDC_FLAG4, OnFlag4)
+	ON_BN_CLICKED(IDC_FLAG5, OnFlag5)
+	ON_BN_CLICKED(IDC_FLAG6, OnFlag6)
+	ON_BN_CLICKED(IDC_FLAG7, OnFlag7)
+	ON_BN_CLICKED(IDC_FLAG8, OnFlag8)
+	ON_EN_KILLFOCUS(IDC_GAMETIME, OnDefaultKillfocus)
+	ON_EN_KILLFOCUS(IDC_GOLD, OnDefaultKillfocus)
+	ON_EN_KILLFOCUS(IDC_WEATHER, OnDefaultKillfocus)
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CGameGeneral message handlers
+
+void CGameGeneral::OnDefaultKillfocus() 
+{
+	UpdateData(UD_RETRIEVE);
+	UpdateData(UD_DISPLAY);
+}
+
+void CGameGeneral::OnFlag1() 
+{
+	the_game.header.weather2^=1;
+	UpdateData(UD_DISPLAY);
+}
+
+void CGameGeneral::OnFlag2() 
+{
+	the_game.header.weather2^=2;	
+	UpdateData(UD_DISPLAY);
+}
+
+void CGameGeneral::OnFlag3() 
+{
+	the_game.header.weather2^=4;	
+	UpdateData(UD_DISPLAY);
+}
+
+void CGameGeneral::OnFlag4() 
+{
+	the_game.header.weather2^=8;
+	UpdateData(UD_DISPLAY);
+}
+
+void CGameGeneral::OnFlag5() 
+{
+	the_game.header.weather2^=0x10;	
+	UpdateData(UD_DISPLAY);
+}
+
+void CGameGeneral::OnFlag6() 
+{
+	the_game.header.weather2^=0x20;	
+	UpdateData(UD_DISPLAY);
+}
+
+void CGameGeneral::OnFlag7() 
+{
+	the_game.header.weather2^=0x40;	
+	UpdateData(UD_DISPLAY);
+}
+
+void CGameGeneral::OnFlag8() 
+{
+	the_game.header.weather2^=0x80;	
+	UpdateData(UD_DISPLAY);
+}
---
+++ GameGeneral.h
@@ -0,0 +1,54 @@
+#if !defined(AFX_GAMEGENERAL_H__FA8C3431_A511_4A4F_8E87_3A6B970A4E9C__INCLUDED_)
+#define AFX_GAMEGENERAL_H__FA8C3431_A511_4A4F_8E87_3A6B970A4E9C__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// GameGeneral.h : header file
+//
+
+/////////////////////////////////////////////////////////////////////////////
+// CGameGeneral dialog
+
+class CGameGeneral : public CDialog
+{
+// Construction
+public:
+	CGameGeneral(CWnd* pParent = NULL);   // standard constructor
+
+// Dialog Data
+	//{{AFX_DATA(CGameGeneral)
+	enum { IDD = IDD_GAMEGENERAL };
+		// NOTE: the ClassWizard will add data members here
+	//}}AFX_DATA
+
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CGameGeneral)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+
+	// Generated message map functions
+	//{{AFX_MSG(CGameGeneral)
+	afx_msg void OnDefaultKillfocus();
+	afx_msg void OnFlag1();
+	afx_msg void OnFlag2();
+	afx_msg void OnFlag3();
+	afx_msg void OnFlag4();
+	afx_msg void OnFlag5();
+	afx_msg void OnFlag6();
+	afx_msg void OnFlag7();
+	afx_msg void OnFlag8();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_GAMEGENERAL_H__FA8C3431_A511_4A4F_8E87_3A6B970A4E9C__INCLUDED_)
